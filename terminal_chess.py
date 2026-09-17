34245b5a9538278fade3aae7dfe11dc56dd9360b
Radosław <raxd3k@gmail.com>
2026-09-17 19:25:49 +0200

    feat: add board displaying

--- terminal_chess.py
+++ terminal_chess.py
@@ -1,1 +1,9 @@
 board = [["#" for _ in range(8)] for _ in range(8)]
+
+def display_board():
+    for row in board:
+        row_string = ""
+        for i in row:
+            row_string += i
+            row_string += " "
+        print(row_string)
