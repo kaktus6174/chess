70e62c1430b85ccfc4dd4443d037cfd354247c13
Radosław <raxd3k@gmail.com>
2026-09-20 16:26:29 +0200

    feat: add starting position setup

--- terminal_chess.py
+++ terminal_chess.py
@@ -7,3 +7,18 @@ def display_board():
             row_string += i
             row_string += " "
         print(row_string)
+
+def set_starting_position():
+    board[0][0], board[0][7] = "r", "r"
+    board[0][1], board[0][6] = "n", "n"
+    board[0][2], board[0][5] = "b", "b"
+    board[0][3], board[0][4] = "q", "k"
+    for i in range(8):
+        board[1][i] = "p"
+
+    board[7][0], board[7][7] = "R", "R"
+    board[7][1], board[7][6] = "N", "N"
+    board[7][2], board[7][5] = "B", "B"
+    board[7][3], board[7][4] = "Q", "K"
+    for i in range(8):
+         board[6][i] = "P"
